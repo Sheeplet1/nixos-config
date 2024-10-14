b4a9dda35b3538b71180633a096247e7e8b7a8a7
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-10-14 18:42:29 +1100

    add: custom vmware-guest.nix for aarch64

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware/macos-vm/hardware-configuration.nix
-      ./modules/vmware-guest.nix
+      ./hardware/vmware-guest.nix
       inputs.home-manager.nixosModules.default
     ];
 
