ce6cb1638379da7f1f791c6cbee1befcd690c76d
Sheeplet <42196548+Sheeplet1@users.noreply.github.com>
2024-10-16 09:38:40 +1100

    add: audio and printing to desktop

--- hardware/x86_64-desktop/extra_configuration.nix
+++ hardware/x86_64-desktop/extra_configuration.nix
@@ -6,4 +6,14 @@
         useOSProber = true;
         efiSupport = true;
     };
+
+    services.printing.enable = true;
+
+    hardware.pulseaudio.enable = false;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
 }
