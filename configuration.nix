8c2763bb4c232cb9993484a90533a67bcaeb4f0f
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2024-10-18 17:28:00 +1100

    enable sway for desktop

--- configuration.nix
+++ configuration.nix
@@ -133,14 +133,27 @@
     # WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
     MOZ_ENABLE_WAYLAND = "1";
-    XDG_SESSION_TYPE = "wayland";
+    # XDG_SESSION_TYPE = "wayland";
+    # XDG_CURRENT_DESKTOP = "Hyprland";
+    # XDG_SESSION_DESKTOP = "Hyprland";
+    # XDG_CONFIG_HOME = "$HOME/.config";
+    # CLUTTER_BACKEND = "wayland";
+    # WLR_DRM_NO_ATOMIC = "1";
+    # # Not sure if this is needed
+    # QT_QPA_PLATFORM = "wayland,x11";
+
+    GBM_BACKEND= "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME= "nvidia";
+    LIBVA_DRIVER_NAME= "nvidia"; # hardware acceleration
+    __GL_VRR_ALLOWED="1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    CLUTTER_BACKEND = "wayland";
+    WLR_RENDERER = "vulkan";
+
     XDG_CURRENT_DESKTOP = "Hyprland";
     XDG_SESSION_DESKTOP = "Hyprland";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    CLUTTER_BACKEND = "wayland";
-    WLR_DRM_NO_ATOMIC = "1";
-    # Not sure if this is needed
-    QT_QPA_PLATFORM = "wayland,x11";
+    XDG_SESSION_TYPE = "wayland";
   };
 
   fonts.packages = with pkgs; [
@@ -188,12 +201,13 @@
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland; # xdphls
     };
 
-    # sway = {
-    #   enable = true;
-    #   wrapperFeatures.gtk = true;
-    # };
+    sway = {
+      enable = true;
+      wrapperFeatures.gtk = true;
+      xwayland.enable = true;
+    };
 
-    # waybar.enable = true;
+    waybar.enable = true;
 
     thunar.enable = true;
     thunar.plugins = with pkgs.xfce; [
@@ -223,11 +237,6 @@
     #             command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
     #         };
     #     };
-    # };
-
-    # displayManager.sddm = {
-    #     enable = true;
-    #     wayland.enable = true;
     # };
 
     xserver = {
