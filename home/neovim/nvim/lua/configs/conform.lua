64c5bba4a8ae27a0f2ac13852a3afca2ed0bc160
Anthony <42196548+Sheeplet1@users.noreply.github.com>
2025-03-22 17:27:34 +1100

    add: biome formatter to conform

--- home/neovim/nvim/lua/configs/conform.lua
+++ home/neovim/nvim/lua/configs/conform.lua
@@ -1,4 +1,14 @@
+local web_formatters = { "biome", "prettierd", stop_after_first = true }
+
 local options = {
+  formatters = {
+    -- Looks through the current working directory for a a configuration file
+    -- and uses this formatter if found.
+    biome = {
+      require_cwd = true,
+    },
+  },
+
   formatters_by_ft = {
     lua = { "stylua" },
     sh = { "shfmt" },
@@ -6,12 +16,12 @@ local options = {
     json = { "prettierd" },
     yaml = { "prettierd" },
 
-    css = { "prettierd" },
-    html = { "prettierd" },
-    javascript = { "prettierd" },
-    javascriptreact = { "prettierd" },
-    typescript = { "prettierd" },
-    typescriptreact = { "prettierd" },
+    css = web_formatters,
+    html = web_formatters,
+    javascript = web_formatters,
+    javascriptreact = web_formatters,
+    typescript = web_formatters,
+    typescriptreact = web_formatters,
 
     python = { "isort", "black", "ruff" },
     go = { "gofumpt", "goimports", "golines" },
