5dc65db4a3afcad5bfeb2a3f1c136fe33ebd4459
Anthony Do <42196548+Sheeplet1@users.noreply.github.com>
2025-07-11 20:54:43 +1000

    change to copilot.vim

--- home/neovim/nvim/lua/plugins/copilot.lua
+++ home/neovim/nvim/lua/plugins/copilot.lua
@@ -1,34 +1,49 @@
 return {
-  "zbirenbaum/copilot.lua",
-  enabled = true,
+  -- "zbirenbaum/copilot.lua",
+  -- enabled = true,
+  -- cmd = "Copilot",
+  -- event = "InsertEnter",
+  -- opts = {
+  --   panel = {
+  --     auto_refresh = false,
+  --     keymap = {
+  --       accept = "<M-l>",
+  --       jump_prev = "[[",
+  --       jump_next = "]]",
+  --       refresh = "gr",
+  --       open = "<M-CR>",
+  --     },
+  --   },
+  --
+  --   suggestion = {
+  --     accept = false,
+  --     auto_trigger = true,
+  --     keymap = {
+  --       accept = "<C-l>",
+  --       prev = "<M-[>",
+  --       next = "<M-]>",
+  --       dismiss = "<C-e>",
+  --     },
+  --   },
+  --
+  --   filetypes = {
+  --     yaml = false,
+  --     markdown = false,
+  --   },
+  -- },
+
+  "github/copilot.vim",
   cmd = "Copilot",
   event = "InsertEnter",
-  opts = {
-    panel = {
-      auto_refresh = false,
-      keymap = {
-        accept = "<M-l>",
-        jump_prev = "[[",
-        jump_next = "]]",
-        refresh = "gr",
-        open = "<M-CR>",
-      },
-    },
-
-    suggestion = {
-      accept = false,
-      auto_trigger = true,
-      keymap = {
-        accept = "<C-l>",
-        prev = "<M-[>",
-        next = "<M-]>",
-        dismiss = "<C-e>",
-      },
-    },
-
-    filetypes = {
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_filetypes = {
       yaml = false,
       markdown = false,
-    },
-  },
+    }
+    vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<M-[>", "copilot#Previous()", { expr = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<M-]>", "copilot#Next()", { expr = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<C-e>", "copilot#Dismiss()", { expr = true, silent = true })
+  end,
 }
