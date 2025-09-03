9926a2eeed8f5579c789c15f7c5c8e9c5f0551fb
Anthony Do <42196548+Sheeplet1@users.noreply.github.com>
2025-09-03 19:35:39 +1000

    update aerospace config

--- darwin/home/aerospace.nix
+++ darwin/home/aerospace.nix
@@ -52,14 +52,15 @@
     workspace-to-monitor-force-assignment = {
       "1" = "secondary";
       "2" = "secondary";
-      "3" = "secondary";
-      "4" = "secondary";
+      "3" = "main secondary";
+      "4" = "main secondary";
       "5" = "main secondary";
       "6" = "main secondary";
       "7" = "main secondary";
     };
 
     on-window-detected = [
+      #  Workspace 1 - Terminals/Coding
       {
         "if".app-id = "com.mitchellh.ghostty";
         run = [
@@ -67,6 +68,8 @@
           "move-node-to-workspace 1"
         ];
       }
+
+      # Workspace 2 - Browsers
       {
         "if".app-id = "com.apple.Safari";
         run = [ "move-node-to-workspace 2" ];
@@ -75,33 +78,42 @@
         "if".app-id = "app.zen-browser.zen";
         run = [ "move-node-to-workspace 2" ];
       }
-      {
-        "if".app-id = "com.todesktop.230313mzl4w4u92";
-        run = [ "move-node-to-workspace 3" ];
-      }
-      {
-        "if".app-id = "com.microsoft.VSCode";
-        run = [ "move-node-to-workspace 3" ];
-      }
+
+      # {
+      #   "if".app-id = "com.todesktop.230313mzl4w4u92";
+      #   run = [ "move-node-to-workspace 3" ];
+      # }
+      # {
+      #   "if".app-id = "com.microsoft.VSCode";
+      #   run = [ "move-node-to-workspace 3" ];
+      # }
+
+      # Workspace 3 - Messages
       {
         "if".app-id = "md.obsidian";
-        run = [ "move-node-to-workspace 4" ];
+        run = [ "move-node-to-workspace 3" ];
       }
+
+      # Messages
       {
         "if".app-id = "com.apple.MobileSMS";
+        run = [ "move-node-to-workspace 4" ];
+      }
+
+      # Spotify
+      {
+        "if".app-id = "com.spotify.client";
         run = [ "move-node-to-workspace 5" ];
       }
+
+      #
       {
         "if".app-id = "com.hnc.Discord";
         run = [ "move-node-to-workspace 6" ];
       }
       {
-        "if".app-id = "com.spotify.client";
-        run = [ "move-node-to-workspace 7" ];
-      }
-      {
         "if".app-id = "com.1password.1password";
-        run = [ "move-node-to-workspace 8" ];
+        run = [ "move-node-to-workspace 7" ];
       }
     ];
   };
